55f7fd42bbb8f929da5ce0c2255e456512f147fb
Arbind <arbind@carbonfive.com>
2013-07-05 08:10:21 -0700

    add configs for redis and geocoder

---
+++ config/initializers/1_redis.rb
@@ -0,0 +1,25 @@
+REDIS_DB_ENVIRONMENTS = {
+  'production'    => 0,
+  :production     => 0,
+
+  'development'   => 1,
+  :'development'  => 1,
+
+  'test'          => 2,
+  :test           => 2
+}
+
+# ensure ENV['RACK_ENV'] is set to 'test', 'development', or 'production 'in order to select a database for the running environment
+REDIS_URI = ENV["REDISTOGO_URL"] || ENV["REDIS_URL"] || "redis://localhost:6379/"
+REDIS_DB = REDIS_DB_ENVIRONMENTS[ ENV["RAILS_ENV"] ]
+
+uri = URI.parse( REDIS_URI )
+
+REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password) rescue nil
+REDIS.select REDIS_DB unless REDIS.nil?
+
+REDIS4GECODER = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password) rescue nil
+REDIS4GECODER.select (3 + REDIS_DB) unless REDIS4GECODER.nil?
+
+puts "\nredis is not running on #{uri}\n!!!" if REDIS.nil?
+puts "\nredis geocoder cache is not running on #{uri}\n!!!" if REDIS4GECODER.nil?
---
+++ config/initializers/2_geocoder.rb
@@ -0,0 +1,11 @@
+# configure geocoder to use redis for cache:
+
+GEOCODER_CACHE_TTL = 86400 # (60s * 60m * 24h)  # +++ TODO move TTL for geo cache into configs
+
+geocoder_config = {
+  lookup: :google,
+  cache: RedisAutoExpire.new(REDIS4GECODER, GEOCODER_CACHE_TTL),
+  cache_prefix: "gO:" # gee-oooh :)
+}
+
+Geocoder.configure geocoder_config
