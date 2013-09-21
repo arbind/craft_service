dc0c4e9a2a4b37d9b4d5abda47b5a80043d79088
Arbind <arbind@carbonfive.com>
2013-09-21 12:09:48 -0700

    don't select a diferent Redis DB in production

--- config/initializers/1_redis.rb
+++ config/initializers/1_redis.rb
@@ -12,6 +12,10 @@ unless $PROGRAM_NAME.end_with?('rake')
     :test           => 2
   }
 
+  def select_redis_db_num(redis, db_num)
+    return if redis.nil? or Rails.env.production? # keep default in production
+    redis.select db_num
+  end
   # ensure ENV['RACK_ENV'] is set to 'test', 'development', or 'production 'in order to select a database for the running environment
   REDIS_URI = ENV["REDISTOGO_URL"] || ENV["REDIS_URL"] || "redis://localhost:6379/"
   REDIS_DB = REDIS_DB_ENVIRONMENTS[ (ENV["RAILS_ENV"] || :development) ]
@@ -19,10 +23,10 @@ unless $PROGRAM_NAME.end_with?('rake')
   uri = URI.parse( REDIS_URI )
 
   REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password) rescue nil
-  REDIS.select REDIS_DB unless REDIS.nil?
+  select_redis_db_num REDIS, REDIS_DB
 
   REDIS4GEOCODER = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password) rescue nil
-  REDIS4GEOCODER.select (3 + REDIS_DB) unless REDIS4GEOCODER.nil?
+  select_redis_db_num REDIS4GEOCODER, 3 + REDIS_DB
 
   puts "\nredis is not running on #{uri}\n!!!" if REDIS.nil?
   puts "\nredis geocoder cache is not running on #{uri}\n!!!" if REDIS4GEOCODER.nil?
