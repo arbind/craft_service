265114497e37a51ec964c93149cd02e1846ad5cb
Arbind <arbind@carbonfive.com>
2013-09-21 11:58:51 -0700

    skip initializers when heroku runs rake assets:precompile

--- config/initializers/1_redis.rb
+++ config/initializers/1_redis.rb
@@ -1,25 +1,29 @@
-REDIS_DB_ENVIRONMENTS = {
-  'production'    => 0,
-  :production     => 0,
+unless $PROGRAM_NAME.end_with?('rake')
+  # Skip this initialization durring rake tasks
+  # Heroku runs rake asset:precompile in sandbox mode with no ENV or DBs
+  REDIS_DB_ENVIRONMENTS = {
+    'production'    => 0,
+    :production     => 0,
 
-  'development'   => 1,
-  :'development'  => 1,
+    'development'   => 1,
+    :'development'  => 1,
 
-  'test'          => 2,
-  :test           => 2
-}
+    'test'          => 2,
+    :test           => 2
+  }
 
-# ensure ENV['RACK_ENV'] is set to 'test', 'development', or 'production 'in order to select a database for the running environment
-REDIS_URI = ENV["REDISTOGO_URL"] || ENV["REDIS_URL"] || "redis://localhost:6379/"
-REDIS_DB = REDIS_DB_ENVIRONMENTS[ (ENV["RAILS_ENV"] || :development) ]
+  # ensure ENV['RACK_ENV'] is set to 'test', 'development', or 'production 'in order to select a database for the running environment
+  REDIS_URI = ENV["REDISTOGO_URL"] || ENV["REDIS_URL"] || "redis://localhost:6379/"
+  REDIS_DB = REDIS_DB_ENVIRONMENTS[ (ENV["RAILS_ENV"] || :development) ]
 
-uri = URI.parse( REDIS_URI )
+  uri = URI.parse( REDIS_URI )
 
-REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password) rescue nil
-REDIS.select REDIS_DB unless REDIS.nil?
+  REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password) rescue nil
+  REDIS.select REDIS_DB unless REDIS.nil?
 
-REDIS4GEOCODER = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password) rescue nil
-REDIS4GEOCODER.select (3 + REDIS_DB) unless REDIS4GEOCODER.nil?
+  REDIS4GEOCODER = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password) rescue nil
+  REDIS4GEOCODER.select (3 + REDIS_DB) unless REDIS4GEOCODER.nil?
 
-puts "\nredis is not running on #{uri}\n!!!" if REDIS.nil?
-puts "\nredis geocoder cache is not running on #{uri}\n!!!" if REDIS4GEOCODER.nil?
+  puts "\nredis is not running on #{uri}\n!!!" if REDIS.nil?
+  puts "\nredis geocoder cache is not running on #{uri}\n!!!" if REDIS4GEOCODER.nil?
+end
